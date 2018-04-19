122e921d614cf18c8649402e545de2910090ac56
Monika Kokot <monikakokot96@gmail.com>
2018-04-19 21:33:45 +0200

    search component

--- FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/SearchTab.java
+++ FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/SearchTab.java
@@ -1,28 +1,80 @@
 package thebestdevelopers.pl.findmybeer;
 
+import android.app.Dialog;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.support.v7.widget.SearchView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+
+import java.util.ArrayList;
 
 import thebestdevelopers.pl.findmybeer.favController.FavTab;
 import thebestdevelopers.pl.findmybeer.mapsController.MapTab;
+import thebestdevelopers.pl.findmybeer.pubInfo.PubInfo;
+import thebestdevelopers.pl.findmybeer.pubList.ItemClickListener;
+import thebestdevelopers.pl.findmybeer.pubList.MyRecyclerViewerAdapter;
+import thebestdevelopers.pl.findmybeer.pubList.Pub;
 
-public class SearchTab extends AppCompatActivity {
+public class SearchTab extends AppCompatActivity implements ItemClickListener {
+
+    private RecyclerView recyclerView;
+    private MyRecyclerViewerAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+    public ArrayList<Pub> pubs;
+    LocationManager lm;
+    Location location;
+    Double longitude;
+    Double latitude;
+    private android.location.LocationListener mLocationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.hide();
+//        ActionBar actionBar = getSupportActionBar();
+//        actionBar.hide();
+        //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
         setContentView(R.layout.activity_search_tab);
         overridePendingTransition(0, 0);
         BottomNavigationView tabs = (BottomNavigationView) findViewById(R.id.navigationtabs3);
         BottomNavigationViewHelper.disableShiftMode(tabs);
         tabs.getMenu().findItem(R.id.action_search).setChecked(true);
+        initializeRecyclerView();
+        // recyclerView.setHasFixedSize(true);
+        initializePubs();
+
+        if (googleServicesAvailable()) {
+            manageLocation();
+
+        } else {
+            Toast.makeText(this, "There's no Google Services installed", Toast.LENGTH_LONG).show();
+        }
+
+
+
+
         Intent i;
         tabs.setOnNavigationItemSelectedListener
                 (new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -56,4 +108,129 @@ public class SearchTab extends AppCompatActivity {
                     }
                 });
     }
+
+
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cannot connect to the play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+    @Override
+    public void onClick(View view, int position) {
+        final Pub currentPub = pubs.get(position);
+        Intent i = new Intent(this, PubInfo.class);
+        i.putExtra("placeID", currentPub.getPlaceID());
+        startActivity(i);
+    }
+
+    private void manageLocation() {
+        Toast.makeText(this, "google services working", Toast.LENGTH_LONG).show();
+        lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        mLocationListener = new android.location.LocationListener() {
+            @Override
+            public void onLocationChanged(final Location location) {
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                mAdapter.updateLocation(location);
+                mAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 10.0f, mLocationListener);
+    }
+
+    private void initializeRecyclerView() {
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(
+                recyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        recyclerView.addItemDecoration(dividerItemDecoration);
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+    private void initializePubs() {
+        pubs = new ArrayList<>();
+        pubs.add(new Pub("trzy siostry", 50.2591173, 19.0266095, 5, 5.0, "ChIJc-kYGzbOFkcRC563sBLpD6w"));
+        pubs.add(new Pub("dubai food", 50.2698693,19.0261198, 4, 4.5, "ChIJJ3UMCiXOFkcRzO760q6SSo0"));
+
+        mAdapter = new MyRecyclerViewerAdapter(pubs);
+        recyclerView.setAdapter(mAdapter);
+        mAdapter.setClickListener(this);
+
+    }
+
+//        @Override
+//    public boolean onSearchRequested() {
+//        //pauseSomeStuff();
+//        return super.onSearchRequested();
+//    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+
+        MenuItem search = menu.findItem(R.id.search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(search);
+        search(searchView);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void search(SearchView searchView) {
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+                mAdapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+    }
+
 }
--- FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/pubList/MyRecyclerViewerAdapter.java
+++ FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/pubList/MyRecyclerViewerAdapter.java
@@ -1,30 +1,26 @@
 package thebestdevelopers.pl.findmybeer.pubList;
 
-    import java.util.Collection;
+    import java.util.ArrayList;
     import java.util.Collections;
-    import java.util.Comparator;
     import java.util.List;
 
-    import android.app.Dialog;
-    import android.content.Context;
-    import android.content.Intent;
     import android.location.Location;
     import android.support.v7.widget.RecyclerView;
     import android.view.LayoutInflater;
     import android.view.View;
-    import android.view.View.OnClickListener;
     import android.view.ViewGroup;
     import android.widget.TextView;
-    import android.widget.Toast;
 
-    import com.google.android.gms.common.ConnectionResult;
-    import com.google.android.gms.common.GoogleApiAvailability;
+    import android.widget.Filter;
+    import android.widget.Filterable;
 
     import thebestdevelopers.pl.findmybeer.R;
-    import thebestdevelopers.pl.findmybeer.pubInfo.PubInfo;
 
-public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerViewerAdapter.ViewHolder>{
+
+
+public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerViewerAdapter.ViewHolder> implements Filterable{
     private List<Pub> pubs;
+    private List<Pub> filteredPubs;
     Double latitude, longitude;
     Location userLocation;
     private ItemClickListener clickListener;
@@ -34,8 +30,7 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
-        // each data item is just a string in this case
-        public View layout;
+
         public TextView textViewStars;
         public TextView textViewPubName;
         public TextView textViewDistance;
@@ -44,7 +39,6 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
 
         public ViewHolder(View v) {
             super(v);
-            layout = v;
             textViewStars = (TextView) v.findViewById(R.id.mTextViewStars);
             textViewPubName = (TextView) v.findViewById(R.id.mTextViewPubName);
             textViewFreeTables = (TextView) v.findViewById(R.id.mTextViewFreeTables);
@@ -76,15 +70,14 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
         userLocation.setLatitude(0.0);
         userLocation.setLongitude(0.0);
         pubs = _pubs;
+        filteredPubs = _pubs;
     }
 
     // Create new views (invoked by the layout manager)
     @Override
     public MyRecyclerViewerAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        // create a new view
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
         View v = inflater.inflate(R.layout.pub_list_item, parent, false);
-        // set the view's size, margins, paddings and layout parameters
         ViewHolder vh = new ViewHolder(v);
         return vh;
     }
@@ -95,7 +88,7 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
         // - get element from your dataset at this position
         // - replace the contents of the view with that element
 
-        final Pub currentPub = pubs.get(position);
+        final Pub currentPub = filteredPubs.get(position);
         Location pubLocation = new Location("pub");
         pubLocation.setLatitude(currentPub.getLatitude());
         pubLocation.setLongitude(currentPub.getLongitude());
@@ -110,7 +103,7 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return pubs.size();
+        return filteredPubs.size();
     }
 
     public void setClickListener(ItemClickListener itemClickListener) {
@@ -118,7 +111,47 @@ public class MyRecyclerViewerAdapter extends RecyclerView.Adapter<MyRecyclerView
     }
     public void updateLocation(Location _location) {
         this.userLocation = _location;
-        Collections.sort(pubs);
+        Collections.sort(filteredPubs);
+    }
+
+        @Override
+    public Filter getFilter() {
+
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+
+                String charString = charSequence.toString();
+
+                if (charString.isEmpty()) {
+
+                    filteredPubs = pubs;
+                } else {
+
+                    ArrayList<Pub> filteredList = new ArrayList<>();
+
+                    for (Pub p : pubs) {
+
+                        if (p.getPubName().toLowerCase().contains(charString)) { //tu mozna ew wsadzic wyszukiwanie po innych rzeczach, np po liczbie wolnych stolikow
+
+                            filteredList.add(p);
+                        }
+                    }
+
+                    filteredPubs = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = filteredPubs;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                filteredPubs = (ArrayList<Pub>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
     }
 
 }
