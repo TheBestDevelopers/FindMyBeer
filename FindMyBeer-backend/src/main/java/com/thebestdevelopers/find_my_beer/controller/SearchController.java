fdb33f457bff38a51b0250d9b0c9a2eb52e8449c
jpisula <jakub.pisula@wp.pl>
2018-05-16 09:25:38 +0200

    a

---
+++ FindMyBeer-backend/src/main/java/com/thebestdevelopers/find_my_beer/controller/SearchController.java
@@ -0,0 +1,13 @@
+package com.thebestdevelopers.find_my_beer.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Dominik Florencki
+ * The Controller to manage user request for searching pub
+ */
+@RestController
+@RequestMapping("api/search/")
+public class SearchController {
+}
