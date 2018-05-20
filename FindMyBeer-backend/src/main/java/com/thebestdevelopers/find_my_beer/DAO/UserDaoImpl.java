6da69ed338e8d88f3658439b4b729d50f62b89b2
Grzegorz863 <gn22@o2.pl>
2018-05-20 22:47:59 +0200

    20.05.2018 przeniesiono całość aplikacji z bazy testowej na produkcyjną

--- FindMyBeer-backend/src/main/java/com/thebestdevelopers/find_my_beer/DAO/UserDaoImpl.java
+++ FindMyBeer-backend/src/main/java/com/thebestdevelopers/find_my_beer/DAO/UserDaoImpl.java
@@ -5,7 +5,7 @@ import com.thebestdevelopers.find_my_beer.model.UserEntity;
 import com.thebestdevelopers.find_my_beer.repository.RoleRepository;
 import com.thebestdevelopers.find_my_beer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 
 /**
@@ -21,24 +21,21 @@ public class UserDaoImpl implements UserDao{
     RoleRepository roleRepository;
 
 
-//
-//    public static String hashPassword(String password_plaintext) {
-//        String salt = BCrypt.gensalt(12);
-//        String hashed_password = BCrypt.hashpw(password_plaintext, salt);
-//
-//        return(hashed_password);
-//    }
+
+    public static String hashPassword(String password_plaintext) {
+        String salt = BCrypt.gensalt(12);
+        String hashed_password = BCrypt.hashpw(password_plaintext, salt);
+
+        return(hashed_password);
+    }
 
     @Override
     public UserEntity createUser(String username, String password, String role) {
 
         UserEntity userEntity = new UserEntity();
         userEntity.setUsername(username);
-        //userEntity.setPassword(hashPassword(password));
-        userEntity.setPassword(password);
-
+        userEntity.setPassword(hashPassword(password));
         userEntity = userRepository.save(userEntity);
-
         RoleEntity roleEntity = new RoleEntity();
         roleEntity.setRole(role);
         roleEntity.setUserId(userEntity.getUserId());
@@ -51,11 +48,11 @@ public class UserDaoImpl implements UserDao{
     @Override
     public Boolean changeUserPassword(String username, String password, String newPassword) {
         UserEntity userEntity = userRepository.findByUsernameAndPassword(username,password).get(0);
-//        if(hashPassword(userEntity.getPassword()).equals(hashPassword(password))) {
-//            userEntity.setPassword(hashPassword(newPassword));
-//            userRepository.save(userEntity);
+        if(hashPassword(userEntity.getPassword()).equals(hashPassword(password))) {
+            userEntity.setPassword(hashPassword(newPassword));
+            userRepository.save(userEntity);
             return true;
-//        } else return false;
+        } else return false;
 
     }
 
