54e252cb151b0952495e700d9a78fc9932076060
Monika Kokot <monikakokot96@gmail.com>
2018-06-15 00:37:31 +0200

    logging authentication. working on keeping session with cookies

--- FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/profileController/ProfileTab.java
+++ FindMyBeer-android/app/src/main/java/thebestdevelopers/pl/findmybeer/profileController/ProfileTab.java
@@ -17,7 +17,7 @@ import android.widget.TextView;
 
 import thebestdevelopers.pl.findmybeer.BottomNavigationViewHelper;
 import thebestdevelopers.pl.findmybeer.HomeTab;
-import thebestdevelopers.pl.findmybeer.Login;
+import thebestdevelopers.pl.findmybeer.loginController.Login;
 import thebestdevelopers.pl.findmybeer.R;
 import thebestdevelopers.pl.findmybeer.favController.FavTab;
 import thebestdevelopers.pl.findmybeer.mapsController.MapTab;
